58affb3ad622ad4db59feb668f7d6b5f04708556
shashidharreddy <kotashashidharreddy1@gmail.com>
2026-01-28 19:52:42 +0530

    explored responsive design for handling all types of devices

--- react-tailwind/src/App.jsx
+++ react-tailwind/src/App.jsx
@@ -2,9 +2,14 @@ import React from 'react'
 
 function App() {
   return (
-    <h1 className="text-4xl font-semibold underline text-red-500">
-      Hello world!
-    </h1>
+    <div className='bg-gray-100 p-4'>
+      <ui className = 'list-none md:flex md:gap-4'>
+        <li className='bg-blue-500 p-2  m-2 rounded-lg'>Item1</li>
+        <li className='bg-blue-500 p-2  m-2 rounded-lg'>Item2</li>
+        <li className='bg-blue-500 p-2  m-2 rounded-lg'>Item3</li>
+      </ui>
+    </div>
+    
   )
 }
 
