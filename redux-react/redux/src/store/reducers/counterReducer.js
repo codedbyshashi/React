22ecf0aa0bed3107772d586935179a6169d16c48
shashidharreddy <kotashashidharreddy1@gmail.com>
2026-02-05 19:06:27 +0530

    .

---
+++ redux-react/redux/src/store/reducers/counterReducer.js
@@ -0,0 +1,18 @@
+const intialState = {
+  count:0,
+}
+
+const counterReducer =(state = intialState,action)=>{
+
+  switch (action.type) {
+    case 'INCREMENT':
+      return {count:state.count+1};
+    case 'DECREMENT':
+      return {count:state.count-1};
+    default:
+      return state;
+  }
+
+};
+
+export default counterReducer;
